a508d63b46f974bba8d18f2010767e953cff70e4
kivilend <65811907+kivilend@users.noreply.github.com>
2020-05-23 16:31:22 +0300

    Add files via upload
    zadanie3

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"1) id потока - {Thread.CurrentThread.ManagedThreadId}");
+
+            List<int> array = new List<int>();
+
+            for (int i = 1; i < 300; i++)
+            {
+                array.Add(i);
+            }
+
+            // параллельная обработка
+            array.AsParallel().ForAll(x =>
+            {
+                double value = Math.Pow(x, 2);
+                Console.WriteLine(
+                    $"Число - {x}, \tвозводим в квадрат - {value}, \tтекущий поток - {Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            Console.WriteLine($"Конец");
+            Console.ReadLine();
+        }
+    }
+}
