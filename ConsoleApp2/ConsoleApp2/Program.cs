18f66467eee663ed6467f1efaf3e35f75d8551f7
kivilend <65811907+kivilend@users.noreply.github.com>
2020-05-23 15:18:04 +0300

    Add files via upload
    zadanie 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Task3().Run();
+        }
+    }
+    public class Task3
+    {
+        public void Run()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            new Task(() => {
+
+                while (!token.IsCancellationRequested)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("Ожидание!");
+                }
+            }).Start();
+            Thread.Sleep(1000);
+            tokenSource.Cancel();
+
+        }
+    }
+}
